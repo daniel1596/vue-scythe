ccfcb8ed78fc1a3c211408e534c12678e079f556
danielmorton3 <32678061+danielmorton3@users.noreply.github.com>
2018-05-30 11:50:00 -0400

    Add everything else except node_modules

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="hello">
+		<h2>{{ titleMessage }}</h2>
+		<h4>Installed CLI Plugins</h4>
+		<ul>
+			<li v-for="(url, title) in urls"> <a :href="url">{{ title }}</a></li>
+		</ul>
+	</div>
+</template>
+
+
+<!-- Add "scoped" keyword to limit CSS to this component only -->
+<style src="./HelloWorld.vue.scss" />
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+class Url {
+	constructor(title: string, url: string) {}
+}
+
+@Component
+export default class HelloWorld extends Vue {
+	private titleMessage: string = "vue app";
+	urls: Url[] = [];
+	
+	constructor() {
+		super();
+		this.urls.push(
+			new Url("Babel", "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"),
+			new Url("TypeScript", "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"),
+			new Url("ESLint", "https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint")
+		);
+	}
+
+}
+</script>
+
+
