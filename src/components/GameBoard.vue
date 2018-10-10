397474903dd5e55ab83afecdb6e384e79659def4
danielmorton3 <32678061+danielmorton3@users.noreply.github.com>
2018-10-09 21:49:58 -0400

    updating the game board (making a couple things instance variables)

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -14,21 +14,21 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 export default class GameBoard extends Vue {
 	width: number = 400; // want to go ahead and make these as attributes of Vue instance/class
 	height: number = 400;
-	// radius: number = 30;
-	// gapBetweenHexesX: number = Math.sqrt(3)*radius/2
-	// gapBetweenHexesY: number = 3*radius/2
-	// factoryOffset: number = 80;
+	radius: number = 30;
+	gapBetweenHexesX: number = Math.sqrt(3)*this.radius/2 
+	gapBetweenHexesY: number = 3*this.radius/2
+	factoryOffset: number = 80; // factory starts at (80, 80) on the canvas
 	
 	
   //@Prop() tile: LandTile;
   // set tile array through game board service
 
   // Note - want to outsource the code to set up the initial game board to something else. (What?)
-	drawHex(x: number, y: number, radius: number) {
+	drawHex(x: number, y: number) {
 		// doc - https://github.com/rrreese/Hexagon.js/blob/master/hexagon.js\
 		// and math here - http://blog.ruslans.com/2011/02/hexagonal-grid-math.html - though I don't want to use "side"
-		let height = Math.sqrt(3) * radius;
-		let width = 2 * radius;
+		let height = Math.sqrt(3) * this.radius;
+		let width = 2 * this.radius;
 	
 		let canvas: any = document.getElementById("gameBoardCanvas");
 		let context = canvas.getContext('2d'); // these two should eventually be outside this method
@@ -36,17 +36,17 @@ export default class GameBoard extends Vue {
 		context.strokeStyle = "#000"; // make this programmatic later
 		context.beginPath();
 		
-		context.moveTo(x, y);
-		// need to create a helper method here for initial offset or something - because I want to start at factoryOffset, factoryOffset but then need offset for the lines, etc.
+		context.moveTo(x, y); // need to use offset value here eventually
+		// need to create a helper method here for initial offset or something - because I want to start at this.factoryOffset, this.factoryOffset but then need offset for the lines, etc.
 		// Since the Canvas itself starts at (0,0) in the top left corner.
 		// but I want the factory to be (0,0)... wahh.
 		
-		context.moveTo(x, y - radius);
-		context.lineTo(x + Math.sqrt(3)*radius/2, y - radius/2);
-		context.lineTo(x + Math.sqrt(3)*radius/2, y + radius/2);
-		context.lineTo(x, y + radius);
-		context.lineTo(x - Math.sqrt(3)*radius/2, y + radius/2);
-		context.lineTo(x - Math.sqrt(3)*radius/2, y - radius/2);
+		context.moveTo(x, y - this.radius);
+		context.lineTo(x + Math.sqrt(3)*this.radius/2, y - this.radius/2);
+		context.lineTo(x + Math.sqrt(3)*this.radius/2, y + this.radius/2);
+		context.lineTo(x, y + this.radius);
+		context.lineTo(x - Math.sqrt(3)*this.radius/2, y + this.radius/2);
+		context.lineTo(x - Math.sqrt(3)*this.radius/2, y - this.radius/2);
 		
 		let fillColor = "#ddd"; // make this programmatic later
 		context.fillStyle =  fillColor;
@@ -59,21 +59,22 @@ export default class GameBoard extends Vue {
   
   mounted() {
     // main game loop - while 1, essentially
-	let radius = 30; // needs to be a member of the class ("this.radius")
-	let factoryOffset = 80; // also should be a member of the class
 	
-	this.drawHex(factoryOffset, factoryOffset, radius);
-	this.drawHex(factoryOffset - (Math.sqrt(3)*radius/2), factoryOffset-(3*radius/2), radius);
-	this.drawHex(factoryOffset + (Math.sqrt(3)*radius/2), factoryOffset-(3*radius/2), radius);
-	this.drawHex(factoryOffset + (Math.sqrt(3)*radius), factoryOffset, radius);
-	this.drawHex(factoryOffset + (Math.sqrt(3)*radius/2), factoryOffset+(3*radius/2), radius);
-	this.drawHex(factoryOffset - (Math.sqrt(3)*radius/2), factoryOffset+(3*radius/2), radius);
-	this.drawHex(factoryOffset - (Math.sqrt(3)*radius), factoryOffset, radius);
+		this.drawHex(this.factoryOffset, this.factoryOffset);
+		this.drawHex(this.factoryOffset - (Math.sqrt(3)*this.radius/2), this.factoryOffset-(3*this.radius/2));
+		this.drawHex(this.factoryOffset + (Math.sqrt(3)*this.radius/2), this.factoryOffset-(3*this.radius/2));
+		this.drawHex(this.factoryOffset + (Math.sqrt(3)*this.radius), this.factoryOffset);
+		this.drawHex(this.factoryOffset + (Math.sqrt(3)*this.radius/2), this.factoryOffset+(3*this.radius/2));
+		this.drawHex(this.factoryOffset - (Math.sqrt(3)*this.radius/2), this.factoryOffset+(3*this.radius/2));
+		this.drawHex(this.factoryOffset - (Math.sqrt(3)*this.radius), this.factoryOffset);
   }
   
-  drawHexesInitial() {}
+  drawHexesInitial() {
+
+	}
   
-  // to make things more readable - use an enum for something like Direction.LEFT, Direction.TOP_LEFT, TOP_RIGHT, RIGHT, etc.
+	// to make things more readable - use an enum for something like Direction.LEFT, 
+	//  Direction.TOP_LEFT, TOP_RIGHT, RIGHT, etc.
   // enums in TypeScript: https://www.typescriptlang.org/docs/handbook/enums.html
 }
 </script>
