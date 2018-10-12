d6d4aefddc9341aa476792ac0381c8ca44977028
danielmorton3 <32678061+danielmorton3@users.noreply.github.com>
2018-10-12 00:11:15 -0400

    removing unused file

--- src/components/GameBoard/TileColor.ts
+++
@@ -1,9 +0,0 @@
-enum TileColor {
-	SILVER = "#ddd",
-	YELLOW = "#aaa"  // not really yellow but can solve that later
-}
-
-export default TileColor; 
-// this is the necessary syntax - for whatever reason, this can't be inlined
-//  i.e. we can't say "export default enum". Don't know why not.
-//  but heck, at least this works... took long enough to figure out.
